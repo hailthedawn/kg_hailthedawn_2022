ddebb02676029e83ead2d56b7cea7bdd8a30fa86
hailthedawn <kkt7778@rit.edu>
2021-05-03 17:26:13 -0400

    Outputs any input number as its phonetic equivalent.

--- main.js
+++ main.js
@@ -2,7 +2,34 @@
 const args = process.argv.slice(2)
 
 var numerical_string = args.map(function(currentValue) {
-	var acc = "Hello World"
+	var acc = ""
+
+	for(var i = 0; i<currentValue.length; i++) {
+		var digit = currentValue.charAt(i);
+		var numerical = ""
+		if(digit=="1")
+			numerical = "One"
+		else if(digit=="2")
+			numerical = "Two"
+		else if(digit=="3")
+			numerical = "Three"
+		else if(digit == "4")
+			numerical = "Four"
+		else if(digit == "5")
+			numerical = "Five"
+		else if(digit == "6")
+			numerical = "Six"
+		else if(digit == "7")
+			numerical = "Seven"
+		else if(digit == "8")
+			numerical = "Eight"
+		else if(digit == "9")
+			numerical = "Nine"
+		else if(digit == "0")
+			numerical = "Zero"
+		acc = acc + numerical
+	}
+
 	return acc
 })
 
