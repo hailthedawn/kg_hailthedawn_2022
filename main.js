4615fa7929ea48528333e4b4edccbb3f4c56fc65
hailthedawn <kkt7778@rit.edu>
2021-05-03 17:24:21 -0400

    Added functionality to map a function to an array

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // get program arguments
 const args = process.argv.slice(2)
-console.log(args)
+
+var numerical_string = args.map(function(currentValue) {
+	var acc = "Hello World"
+	return acc
+})
+
+console.log(numerical_string)
